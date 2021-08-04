6e2ac8831d13665a27c089facf96b18c23031035
technologicza <jean.louw@technologic.za.net>
2021-08-04 09:56:00 +0200

    Add files via upload

---
+++ Remove-UserSIDHistory.ps1
@@ -0,0 +1,23 @@
+﻿$AllSIDUsers = gc .\SIDUsers.txt
+
+foreach ($User in $AllSIDUsers){
+
+$UserValue = $User
+
+$SIDValues = (Get-ADUser -Identity $UserValue -Properties SidHistory | Select-Object -ExpandProperty SIDHistory).Value
+
+if ($SIDValues -ne $null){
+
+foreach ($Value in $SIDValues){
+Write-Host "..removing" $Value "from" $UserValue -ForegroundColor Red
+Set-ADUser -Identity $UserValue -Remove @{SIDHistory=$Value}
+}
+}
+
+else{
+Write-Host "..skipping" $UserValue "as it has no SIDHistory" -ForegroundColor Green
+}
+If (Test-Path variable:UserValue){Remove-Variable UserValue}
+If (Test-Path variable:Value){Remove-Variable Value}
+If (Test-Path variable:SIDValues){Remove-Variable SIDValues}
+}
